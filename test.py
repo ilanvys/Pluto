7f51df3497dc002de3de7b14c8c12b9a60dda6d3
annarubtsov <anna.rubtsov@mail.huji.ac.il>
2023-05-31 20:26:14 +0300

    Fix test.py

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-print("Pluto!")
-print("shani")
-print("secfgfgound try")
-=======
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
