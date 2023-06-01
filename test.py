10d5f3b1d0ef5e8bc2990155a064caada2075288
user <sahar.aharon@mail.huji.ac.il>
2023-06-01 17:47:53 +0300

    done

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ import requests
 import json
 import openai
 import os
+import time
 pdf_latex =r"""\documentclass[10pt]{article}
 \usepackage[utf8]{inputenc}
 \usepackage[T1]{fontenc}
@@ -65,17 +66,74 @@ $$
 
 
 \end{document}"""
-def req_from_chat(question):
-    # openai.api_key = os.getenv("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")
-    openai.api_key = "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"
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": question
-             }
-        ]
-    )
-    return completion.choices[0].message
+pdf_url_global = "https://ocw.mit.edu/courses/18-06-linear-algebra-spring-2010/e9bbec7a9a25a87715e8edd75e21d7b9_MIT18_06S10_exam1_s10.pdf"
+app_id = "sahar_aharon_mail_huji_ac_il_3c7cbb_0e2a12"
+app_key = "4f4bc73d80dd02413096cb9d6bc8ea8e9c17936956fda1e669f944e484f17b99"
+def req_from_mathpix(pdf_url):
+    app_id = "sahar_aharon_mail_huji_ac_il_3c7cbb_0e2a12"
+    app_key = "4f4bc73d80dd02413096cb9d6bc8ea8e9c17936956fda1e669f944e484f17b99"
+    url = "https://api.mathpix.com/v3/pdf"
+    payload = {
+        "url": pdf_url,
+        "conversion_formats": {
+            "docx": True,
+            "tex.zip": True
+        }
+    }
+    headers = {
+        "app_id": app_id,
+        "app_key": app_key,
+        "Content-type": "application/json"
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    pdf_id = response.json()["pdf_id"]
+
+    response = requests.get(url + "/" + pdf_id , headers = headers)
+    while(response.json()["status"] != "completed") :
+        time.sleep(1)
+        response = requests.get(url + "/" + pdf_id , headers = headers)
+    # print("Processing done!")
+
+
+    response = requests.get( "https://api.mathpix.com/v3/converter/" + pdf_id , headers= headers)
+    while(response.json()["conversion_status"]["tex.zip"]["status"] != "completed") :
+        time.sleep(1)
+        response = requests.get("https://api.mathpix.com/v3/converter/" + pdf_id, headers=headers)
+    # print("Processing latex done!")
+
+
+    headers = {
+        "app_key": app_key,
+        "app_id": app_id
+    }
+    url = f"https://api.mathpix.com/v3/pdf/{pdf_id}.tex"
+    response = requests.get(url, headers=headers)
+    # print("all done!")
+    text = response.text
+
+    index = text.find("\\documentclass")
+    if index != -1:
+        cleaned_text = text[index:]
+    else:
+        cleaned_text = text
+
+    cleaned_text_index = text.find("end{document}")
+    if index != -1:
+        cleaned_text_end = cleaned_text[:cleaned_text_index + len("end{document}") ]
+    else:
+        cleaned_text_end = cleaned_text
+    return cleaned_text_end
+
+    # openai.api_key = "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"
+    # completion = openai.ChatCompletion.create(
+    #     model="gpt-3.5-turbo",
+    #     messages=[
+    #         {"role": "user", "content": question
+    #          }
+    #     ]
+    # )
+    # return completion.choices[0].message
 
 # def req_from_mathpix(pdf):
 #     # openai.api_key = os.getenv("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")
@@ -102,6 +160,7 @@ def req_from_chat(question):
 # openai.api_key = os.getenv("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")
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(req_from_chat("can you make a new test at the same level as this one :"+pdf_latex).content)
+    pdf_url_global = "https://ocw.mit.edu/courses/18-06-linear-algebra-spring-2010/e9bbec7a9a25a87715e8edd75e21d7b9_MIT18_06S10_exam1_s10.pdf"
+    print(req_from_mathpix(pdf_url_global))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
